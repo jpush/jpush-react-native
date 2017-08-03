e59d47323e2cbcbddfc5f99b432d41cacd3a5ce3
messense <messense@icloud.com>
2017-08-03 10:25:04 +0800

    Remove createJSModules for RN 0.47

--- android/src/main/java/cn/jpush/reactnativejpush/JPushPackage.java
+++ android/src/main/java/cn/jpush/reactnativejpush/JPushPackage.java
@@ -25,11 +25,6 @@ public class JPushPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
