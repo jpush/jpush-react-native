5a05e6f4110d0f2b758013e351b6230067a31429
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2017-09-20 14:53:13 +0800

    add sendLocalNotification

--- index.d.ts
+++ index.d.ts
@@ -257,6 +257,20 @@ declare class JPush {
     ): void;
 
     /**
+	 * @param {Object} notification = {
+	 	  'buildId': Number     // 设置通知样式，1 为基础样式，2 为自定义样式。自定义样式需要先调用 setStyleCustom 接口设置自定义样式。
+	 *    'id': Number    		// 通知的 id, 可用于取消通知
+	 *    'title': String 		// 通知标题
+	 *    'content': String  	// 通知内容
+	 *	  'extra': Object       // extra 字段
+	 *    'broadcastTime': String  // 通知触发时间 格式： 'yyyy-MM-dd HH:mm:ss'  broadcastTime 和 delay 两者选填一个即可。
+	 *    'delay': Number  // 秒，延迟多少秒后触发本地通知。delay 和 broadcastTime 两者之间选填一个即可。
+	 *    
+	 *  }
+	 */
+    static sendLocalNotification(notification: Object): void;
+
+    /**
      * iOS Only
      * 设置应用 Badge（小红点）
      */
