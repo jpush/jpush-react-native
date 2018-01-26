4d10fa8f86f4b8e4c1b4668dd7ffce679e916a72
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2018-01-26 11:22:44 +0800

    fix demo bug

--- example/second.js
+++ example/second.js
@@ -5,83 +5,26 @@ import JPushModule from 'jpush-react-native'
 const { View, Text, TouchableHighlight, StyleSheet } = ReactNative
 
 export default class Second extends React.Component {
-  onBackPress = () => {
-    if (!this.props.navigator) {
-      this.props.navigator.pop()
-    } else {
-      console.log('finishing second activity')
-      JPushModule.finishActivity()
-    }
-  }
 
-  componentDidMount () {
+  componentDidMount() {
+    // Receive extra 
     JPushModule.addReceiveExtrasListener(map => {
       console.log('Got extra, key: hello, value: ' + map.hello)
     })
   }
 
-  onButtonPress = () => {
-    console.log('will jump to setting page')
-    if (!this.props.navigator) {
-      this.props.navigator.push({
-        name: 'setActivity'
-      })
-    } else {
-    }
-  }
-
-  render () {
+  render() {
     return (
       <View>
-        <TouchableHighlight
-          style={styles.backBtn}
-          underlayColor='#e4083f'
-          activeOpacity={0.5}
-          onPress={this.onBackPress}
-        >
-          <Text>Back</Text>
-        </TouchableHighlight>
         <Text style={styles.welcome}>Welcome !</Text>
-        <TouchableHighlight
-          underlayColor='#e4083f'
-          activeOpacity={0.5}
-          style={styles.btnStyle}
-          onPress={this.onButtonPress}
-        >
-          <Text style={styles.btnTextStyle}>Jump To Setting page!</Text>
-        </TouchableHighlight>
       </View>
     )
   }
 }
 
 var styles = StyleSheet.create({
-  backBtn: {
-    padding: 10,
-    marginTop: 10,
-    marginLeft: 10,
-    borderWidth: 1,
-    borderColor: '#3e83d7',
-    backgroundColor: '#3e83d7',
-    borderRadius: 8,
-    alignSelf: 'flex-start'
-  },
   welcome: {
     textAlign: 'center',
     margin: 10
   },
-  btnStyle: {
-    marginTop: 10,
-    borderWidth: 1,
-    borderColor: '#3e83d7',
-    borderRadius: 8,
-    backgroundColor: '#3e83d7',
-    alignSelf: 'center',
-    justifyContent: 'center'
-  },
-  btnTextStyle: {
-    textAlign: 'center',
-    fontSize: 25,
-    color: '#ffffff'
-  }
 })
