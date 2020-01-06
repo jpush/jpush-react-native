2f6f1d70d081e94a19f2dad4b36b9c4ab816d161
zhanq <zhanq@jpush.cn>
2020-01-06 10:21:59 +0800

    更新版本号到2.7.5：支持ts

--- index.d.ts
+++ index.d.ts
@@ -408,11 +408,11 @@ export default class JPush {
    */
   static setBadge(params: {
     /**
-     * Push封装badge功能,允许应用上传 badge 值至 JPush 服务器,由 JPush 后台帮助管理每个用户所对应的推送 badge 值,简化了设置推送 badge 的操作。
+     * Push封装badge功能,允许应用上传 badge 值至 JPush 服务器,由 JPush 后台帮助管理每个用户所对应的推送 badge 值,简化了设置推送 badge 的操作。设置的值小于0时，sdk时不作处理。
      */
     badge: number;
     /**
-     * iOS 用来标记应用程序状态的一个数字,出现在程序图标右上角。
+     * iOS 用来标记应用程序状态的一个数字,出现在程序图标右上角。设置的值小于0时，sdk不作处理。
      */
     appBadge: number;
   }): void;
